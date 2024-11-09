9ff4c2bfa0913e555ea55b888e52c10247b53e17
Azamat Gilmanshin <ttapent@gmail.com>
2024-11-09 22:04:46 +0500

    Добавьте файлы проекта.

---
+++ AddEditPage.xaml.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KursovayaRabota
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Литература _currentLit = new Литература();
+        public AddEditPage( Литература SelectedLit)
+        {
+            InitializeComponent();
+            if (SelectedLit != null)
+            {
+                _currentLit = SelectedLit;
+            }
+            DataContext = _currentLit;
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new BooksPage());
+        }
+
+        private void Picture_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _currentLit.Image = openFileDialog.FileName;
+                ImageLit.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+        public static bool IsValidYear(string year)
+        {
+            string pattern = @"[1234567890]{4}$";
+            return Regex.IsMatch(year, pattern);
+        }
+        public static bool IsValidPage(string page)
+        {
+            string pattern = @"[1234567890]$";
+            return Regex.IsMatch(page, pattern);
+        }
+        public static bool IsValidZal(string zal)
+        {
+            string pattern = @"[1234567890]$";
+            return Regex.IsMatch(zal, pattern);
+        }
+        public static bool IsValidEdLit(string EdLit)
+        {
+            string pattern = @"[1234567890]$";
+            return Regex.IsMatch(EdLit, pattern);
+        }
+
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentLit.Название))
+            {
+                errors.AppendLine("Укажите Название");
+            }
+            if (string.IsNullOrWhiteSpace(_currentLit.Категория_литературы))
+            {
+                errors.AppendLine("Укажите Категорию");
+            }
+            if (string.IsNullOrWhiteSpace(_currentLit.Авторы))
+            {
+                errors.AppendLine("Укажите Авторов");
+            }
+            if (string.IsNullOrWhiteSpace(_currentLit.Издательство))
+            {
+                errors.AppendLine("Укажите Издательство");
+            }
+            if (string.IsNullOrWhiteSpace(_currentLit.Год_издательства.ToString()))
+            {
+                errors.AppendLine("Укажите Год издательства");
+            }
+            if (string.IsNullOrWhiteSpace(_currentLit.Кол_во_страниц.ToString()))
+            {
+                errors.AppendLine("Укажите Кол-во страниц");
+            }
+            if (string.IsNullOrWhiteSpace(_currentLit.Читальный_зал_ID.ToString()))
+            {
+                errors.AppendLine("Укажите Читальный зал ID");
+            }
+            if (string.IsNullOrWhiteSpace(_currentLit.Кол_во_единиц_лит_ры.ToString()))
+            {
+                errors.AppendLine("Укажите Кол-во единиц литературы");
+            }
+            if (!IsValidYear(YearIzd.Text))
+            {
+                errors.AppendLine("Укажите год издательства правильно(4 цифры)");
+            }
+            if (!IsValidPage(CountPage.Text))
+            {
+                errors.AppendLine("Укажите кол-во страниц корректно");
+            }
+            if (!IsValidZal(ChitalID.Text))
+            {
+                errors.AppendLine("Укажите ID читального зала корректно ");
+            }
+            if (!IsValidEdLit(CountLit.Text))
+            {
+                errors.AppendLine("Укажите кол-во единиц литературы корректно");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentLit.ID_литературы == 0)
+            {
+                CityLibraryEntities.GetContext().Литература.Add(_currentLit);
+            }
+            try
+            {
+                CityLibraryEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                
+            }
+
+        }
+    }
+}
---
+++ BooksPage.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KursovayaRabota
+{
+    /// <summary>
+    /// Логика взаимодействия для BooksPage.xaml
+    /// </summary>
+    public partial class BooksPage : Page
+    {
+        int CountRecords = CityLibraryEntities.GetContext().Литература.Count();
+        public BooksPage()
+        {
+            InitializeComponent();
+            var currentBooks = CityLibraryEntities.GetContext().Литература.ToList();
+            BooksListView.ItemsSource = currentBooks;
+            TBallRecords.Text = "Записей: " + CountRecords + " из " + CountRecords;
+        }
+        
+        
+        private void UpdateBooks()
+        {
+            var currentBooks = CityLibraryEntities.GetContext().Литература.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentBooks = currentBooks/*.Where(p => (p.Категория_литературы == "Художественная")).ToList()*/;
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentBooks = currentBooks.Where(p => (p.Категория_литературы == "Художественная")).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentBooks = currentBooks.Where(p => (p.Категория_литературы == "Фантастика")).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentBooks = currentBooks.Where(p => (p.Категория_литературы == "Научная")).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentBooks = currentBooks.Where(p => (p.Категория_литературы == "Научно-популярная")).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentBooks = currentBooks.Where(p => (p.Категория_литературы == "Образовательная")).ToList();
+            }
+            if (ComboType.SelectedIndex == 6)
+            {
+                currentBooks = currentBooks.Where(p => (p.Категория_литературы == "Практическая")).ToList();
+            }
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentBooks = currentBooks.OrderByDescending(p => p.Кол_во_страниц).ToList();
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentBooks = currentBooks.OrderBy(p => p.Кол_во_страниц).ToList();
+            }
+
+            var CountRecordsNew = currentBooks.Count;
+            TBallRecords.Text = "Записей: " + CountRecordsNew + " из " + CountRecords;
+
+            
+            currentBooks = currentBooks.Where(p => p.Название.ToLower().Contains(TboxSearch.Text.ToLower())).ToList();
+            BooksListView.ItemsSource = currentBooks;
+        }
+
+        private void TboxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateBooks();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateBooks();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateBooks();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateBooks();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (((sender as Button).DataContext as Литература).Кол_во_единиц_лит_ры == 0)
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    CityLibraryEntities.GetContext().Литература.Remove((sender as Button).DataContext as Литература);
+                    CityLibraryEntities.GetContext().SaveChanges();
+
+                    BooksListView.ItemsSource = CityLibraryEntities.GetContext().Литература.ToList();
+                    CountRecords -= 1; 
+                    UpdateBooks();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Невозможно удалить", "Ошибка");
+            }
+        }
+
+        private void DeleteFilter_Click(object sender, RoutedEventArgs e)
+        {
+            RButtonDown.IsChecked = false;
+            RButtonUp.IsChecked = false;
+            UpdateBooks();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Литература));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                CityLibraryEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                BooksListView.ItemsSource = CityLibraryEntities.GetContext().Литература.ToList();
+            }
+            CountRecords = CityLibraryEntities.GetContext().Литература.Count();
+            UpdateBooks();
+        }
+    }
+}
